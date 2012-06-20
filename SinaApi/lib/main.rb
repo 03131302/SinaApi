513e4d66b94115fa78723dab3edfca9ff58d569c
Administrator <03131302@163.com>
2012-06-20 10:01:56 +0800

    提交

--- SinaApi/lib/main.rb
+++ SinaApi/lib/main.rb
@@ -19,11 +19,11 @@ class CreateInfoTool
     @app = DoubanApp.new
 
     #电影
-    @data = @app.parse(@app.access_token.get("/movie/subjects?tag=%E7%8A%AF%E7%BD%AA&start-index=260&max-results=10&alt=json").body)
+    @data = @app.parse(@app.access_token.get("/movie/subjects?tag=%E7%8A%AF%E7%BD%AA&start-index=270&max-results=10&alt=json").body)
     #书
-    @data_books = @app.parse(@app.access_token.get("/book/subjects?tag=%E5%B0%8F%E8%AF%B4&start-index=260&max-results=10&alt=json").body)
+    @data_books = @app.parse(@app.access_token.get("/book/subjects?tag=%E5%B0%8F%E8%AF%B4&start-index=270&max-results=10&alt=json").body)
     #歌
-    @data_musices = @app.parse(@app.access_token.get("/music/subjects?tag=%E6%B0%91%E8%B0%A3&start-index=260&max-results=10&alt=json").body)
+    @data_musices = @app.parse(@app.access_token.get("/music/subjects?tag=%E6%B0%91%E8%B0%A3&start-index=270&max-results=10&alt=json").body)
 
     @sina_app = SinaApp.new
   end
