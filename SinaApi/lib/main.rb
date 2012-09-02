572940c3f26171dca20c70eecf5e0e788fd354fa
YnagXD <03131302@163.com>
2012-09-02 11:02:09 +0800

    提交

--- SinaApi/lib/main.rb
+++ SinaApi/lib/main.rb
@@ -13,18 +13,18 @@ class CreateInfoTool
 
   def initialize
     Weibo::Config.img_dir = "D:/douban_image/"
-    Weibo::Config.is_proxy_use = true
+    Weibo::Config.is_proxy_use = false
     Weibo::Config.is_to_gbk = false
     Weibo::Config.proxy = "http://yangxd:my003131302@nproxy.slof.com:80"
-    @app = DoubanApp.new
-
-    #电影
-    @data = @app.parse(@app.access_token.get("/movie/subjects?tag=%E7%8A%AF%E7%BD%AA&start-index=320&max-results=10&alt=json").body)
-    #书
-    @data_books = @app.parse(@app.access_token.get("/book/subjects?tag=%E5%B0%8F%E8%AF%B4&start-index=320&max-results=10&alt=json").body)
-    #歌
-    @data_musices = @app.parse(@app.access_token.get("/music/subjects?tag=%E6%B0%91%E8%B0%A3&start-index=320&max-results=10&alt=json").body)
-
+=begin
+          @app = DoubanApp.new
+          #电影
+          @data = @app.parse(@app.access_token.get("/movie/subjects?tag=%E7%8A%AF%E7%BD%AA&start-index=360&max-results=10&alt=json").body)
+          #书
+          @data_books = @app.parse(@app.access_token.get("/book/subjects?tag=%E5%B0%8F%E8%AF%B4&start-index=360&max-results=10&alt=json").body)
+          #歌
+          @data_musices = @app.parse(@app.access_token.get("/music/subjects?tag=%E6%B0%91%E8%B0%A3&start-index=360&max-results=10&alt=json").body)
+=end
     @sina_app = SinaApp.new
   end
 
--- SinaApi/lib/sina_app_aouth.rb
+++ SinaApi/lib/sina_app_aouth.rb
@@ -14,6 +14,7 @@ require File.join(directory, 'weibo', 'httpauth')
 require File.join(directory, 'weibo', 'request')
 require File.join(directory, 'weibo', 'config')
 require File.join(directory, 'weibo', 'base')
+require File.expand_path('../weibo_2', __FILE__)
 
 class SinaApp
 
@@ -22,33 +23,32 @@ class SinaApp
 # @param api_secret [Object]
   def initialize(api_key="615201284", api_secret="c824df11a6e95ef3be83d60725ec1c0e")
 
-    Weibo::Config.api_key = api_key
-    Weibo::Config.api_secret = api_secret
-    @oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
+    WeiboOAuth2::Config.api_key = api_key
+    WeiboOAuth2::Config.api_secret = api_secret
+    WeiboOAuth2::Config.redirect_uri = "http://127.0.0.1:4567/callback"
+    @oauth = WeiboOAuth2::Client.new('', '', :ssl => {:ca_path => "D:/Ruby/opentool/openssl/certs"})
 
-    # 1. 获取授权路径
-    @request_token = oauth.consumer.get_request_token
-
-    # 2. 用户授权应用，获取授权码
+    # 1. 用户授权应用，获取授权码
     puts Weibo::Config.to_gbk("拷贝URL到浏览器进行授权:")
-    puts @request_token.authorize_url
-    key = init_sina_login @request_token.authorize_url
+    puts @oauth.authorize_url
+    key = gets #init_sina_login @oauth.authorize_url
     puts Weibo::Config.to_gbk("请输入获取到的授权码：#{key}")
-    @oauth_verifier = key
-    puts Weibo::Config.to_gbk("获取到的授权码是：#{@oauth_verifier[0, 6]}，#{@oauth_verifier == key}")
-
-    # 3. 获取授权
-    @oauth.authorize_from_request(@request_token, @oauth_verifier[0, 6])
+    @oauth_verifier = key[0,32]
+    puts Weibo::Config.to_gbk("获取到的授权码是：#{@oauth_verifier}，#{@oauth_verifier == key[0,32]}")
+    @oauth.auth_code.get_token(@oauth_verifier)
   end
 
   def init_sina_login(url)
     begin
       browser = Watir::Browser.new :firefox
+      browser.driver.manage.timeouts.implicit_wait = 10
       browser.goto url
       browser.text_field(:id, "userId").set("03131302@163.com")
       browser.text_field(:id, "passwd").set("the_003131302")
-      browser.link(:id, "sub").click
-      thekey = browser.span(:class, "fb").text
+      browser.link(:class, "WB_btn_oauth formbtn_01").click
+      browser.link(:class, "WB_btn_oauth formbtn_01").wait_while_present
+      Watir::Wait.until { browser.url.include? '127.0.0.1' }
+      thekey = browser.url.to_s[browser.url.to_s.length-32, browser.url.to_s.length]
     rescue => err
       puts err
       puts Weibo::Config.to_gbk("请在确认之后输入验证码：")
@@ -58,13 +58,14 @@ class SinaApp
 
   #提交文本型微博内容
   def update(status)
-    Weibo::Base.new(@oauth).update(status)
+    @oauth.statuses.update(CGI.escape(status))
   end
 
   #提交图片信息的微博，传入文件路径
   def upload(status, filedir)
     begin
-      Weibo::Base.new(@oauth).upload(CGI.escape(status), File.new(filedir, "rb"))
+      pic = File.open(filedir)
+      @oauth.statuses.upload(CGI.escape(status), pic)
     rescue => err
       puts err
     end
